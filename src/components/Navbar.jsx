c519ad2855fc2fbcef997695ac10dc50eb6cef69
SUBHAKSHAN CHAKRABORTY <167389887+Subhakshan-Chakraborty@users.noreply.github.com>
2024-10-03 17:12:18 +0530

    blue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 export default function Navbar() {
   return (
     <div>
-      <h1 className="bg-black text-white text-center text-5xl py-5">
+      <h1 className="bg-blue-700 text-white text-center text-5xl py-5">
         Total Health
       </h1>
     </div>
